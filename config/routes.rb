ce505b62715ca0addbfb4905c03e40b637c280d5
hpneo <hpneo@hotmail.com>
2017-09-17 15:47:50 -0500

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :product_types
   devise_for :users
   post '/login' => 'login#login'
+
+  root to: redirect('/admin')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
